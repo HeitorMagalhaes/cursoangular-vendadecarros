8f5cabcda67cf947508f28994400b702e08c2dce
Heitor Magalhães <heitor.magalhaes@rhitmo.com.br>
2023-07-13 08:59:36 -0300

    Aula final disponível - Projeto não terminado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { AuthGuardService } from './resource/services/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', component: LoginComponent}
+  { path: '', component: LoginComponent },
+  {
+    path: 'dashboard',
+    canActivate: [AuthGuardService],
+    loadChildren: () =>
+      import('./views/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RequestLogin } from 'src/app/resource/models/RequestLogin';
+import { AlertService } from 'src/app/resource/services/alert.service';
 import { LoginService } from 'src/app/resource/services/login.service';
 
 @Component({
@@ -8,21 +10,27 @@ import { LoginService } from 'src/app/resource/services/login.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  public requestLogin!: RequestLogin ;
+  public requestLogin!: RequestLogin;
 
-  constructor(private loginService:LoginService) {}
+  constructor(
+    private loginService: LoginService,
+    private alertService: AlertService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
   }
 
-  public doLogin() : void{
-    this.loginService.doLogin(this.requestLogin).subscribe(data=>{
-      console.log(data);
-    },
-    error => {
-      console.error(error);
-    }
+  public doLogin(): void {
+    this.loginService.doLogin(this.requestLogin).subscribe(
+      (data) => {
+        this.router.navigate(["dashboard"]);
+      },
+      (httpError) => {
+        this.alertService.error(httpError.error.message);
+        console.error(httpError);
+      }
     );
   }
 }
